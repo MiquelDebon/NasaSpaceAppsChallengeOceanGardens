d5a1e9d6caea0e38d500024fcdae2b2f4794af40
Miquel Debón Villagrasa <mdebonbcn@gmail.com>
2023-10-07 18:02:42 +0200

    connenting frontend

--- src/main/java/com/galacticspacecoders/hackathon/controller/AuthController.java
+++ src/main/java/com/galacticspacecoders/hackathon/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.galacticspacecoders.hackathon.controller;
 
 import com.galacticspacecoders.hackathon.model.dto.LoginDto;
 import com.galacticspacecoders.hackathon.model.dto.RegisterDto;
+import com.galacticspacecoders.hackathon.model.dto.UserDto;
 import com.galacticspacecoders.hackathon.model.services.AuthenticationService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -9,10 +10,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "PHYTOPLANKTON Authentication")
 @RestController
@@ -37,7 +35,7 @@ public class AuthController {
             }
     )
     @PostMapping("/register")
-    public ResponseEntity<?> register(
+    public ResponseEntity<UserDto> register(
             @RequestBody RegisterDto registerDto
     ){
         return ResponseEntity.ok().body(service.registerUser(registerDto));

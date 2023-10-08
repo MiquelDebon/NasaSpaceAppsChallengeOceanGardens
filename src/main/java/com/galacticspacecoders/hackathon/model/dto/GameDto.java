f7188d1598afb5e5dd4ab5cf373a3f0cf2b3fb41
montseliz <montse.liz92@gmail.com>
2023-10-08 10:50:34 +0200

    style corrections

--- src/main/java/com/galacticspacecoders/hackathon/model/dto/GameDto.java
+++ src/main/java/com/galacticspacecoders/hackathon/model/dto/GameDto.java
@@ -1,13 +1,20 @@
 package com.galacticspacecoders.hackathon.model.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 public class GameDto {
+
+    @Schema(description = "Identification of the user playing", example = "643d909f15da8348ee4805c1")
     private String id;
+
+    @Schema(description = "Identification of the functionality to play", example = 1)
     private int action;
+
+    @Schema(description = "Response of the operation", example = true)
     private boolean answer;
 
     public int getAction() {
--- src/main/java/com/galacticspacecoders/hackathon/model/exception/customExceptions/DuplicateUserNameException.java
+++ src/main/java/com/galacticspacecoders/hackathon/model/exception/customExceptions/DuplicateUserNameException.java
@@ -4,7 +4,6 @@ public class DuplicateUserNameException extends RuntimeException{
     public DuplicateUserNameException() {
         super("Duplicate user name");
     }
-
     public DuplicateUserNameException(String message) {
         super(message);
     }
